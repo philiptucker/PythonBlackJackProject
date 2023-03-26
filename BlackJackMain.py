06f357897bcb3f3bcc8d5e02e860985a119428bb
philiptucker <philip.tucker37@ed.cna.nl.ca>
2023-03-26 01:11:15 -0400

    "Implemented blackjack rules, added hit/stand function for player and dealer"

--- BlackJackMain.py
+++ BlackJackMain.py
@@ -86,6 +86,69 @@ def deckBuild():
     return deck
 
 
+def playerBlackJack(playerPoints):
+    #TODO Implement into Code
+    if playerPoints == 21:
+        return True
+    else:
+        return False
+
+
+def dealerBlackJack(dealerPoints):
+    #TODO Implement into Code
+    if dealerPoints == 21:
+        return True
+    else:
+        return False
+
+
+def hitStand(deck, playerHand):
+    while True:
+        draw = input("\nHit or stand? (hit/stand): ")
+        if draw.lower() == "hit":
+            playerPoints = 0
+            dealCard = deck.pop(0)
+            print("\nYOUR CARDS:")
+            for card in playerHand:
+                print(f"{card[1]} of {card[0]}")
+                playerPoints += card[2]
+            print(f"{dealCard[1]} of {dealCard[0]}")
+            if dealCard[2] == 11 & playerPoints > 10:
+                dealCard.pop(2)
+                dealCard.append(1)
+            elif dealCard[2] == "11":
+                choice = input("Would you like this to be a 1 or an 11? ")
+                while choice != "1" or choice != "11":
+                    print("Must select 1 or 11. Please try again")
+                if choice == "1":
+                    dealCard.pop(2)
+                    dealCard.append(1)
+            playerHand.append(dealCard)
+            if playerPoints > 21:
+                print("BUST!")
+
+        elif draw.lower() == "stand":
+            break
+        else:
+            print("Invalid response. Please try again.")
+
+
+def dealerHitStand(deck, dealerHand, dealerPoints):
+    while dealerPoints < 17:
+        print("\nDealer Hits")
+        dealCard = deck.pop(0)
+        if dealCard[2] == 11 & dealerPoints > 10:
+            dealCard.pop(2)
+            dealCard.append(1)
+        dealerPoints += dealCard[2]
+        dealerHand.append(dealCard)
+        print("\nDEALER'S CARDS:")
+        for card in dealerHand:
+            print(f"{card[1]} of {card[0]}")
+        if dealerPoints > 21:
+            print("DEALER BUST!")
+
+
 def pointCheck(playerHand, dealerHand, money, betAmount):
     playerPoints = 0
     dealerPoints = 0
@@ -96,9 +159,11 @@ def pointCheck(playerHand, dealerHand, money, betAmount):
     print(f"\nYOUR POINTS: \t\t{playerPoints}"
           f"\nDEALER'S POINTS: \t{dealerPoints}\n")
 
-    if playerPoints > dealerPoints:
+    if playerPoints > dealerPoints & playerPoints <= 21:
         print("Congrats, you win!")
         money += betAmount
+    elif playerPoints == dealerPoints & playerPoints <= 21:
+        print("Tie game. Return bet.")
     else:
         print("Sorry. You lose.")
         money -= betAmount
@@ -110,15 +175,20 @@ def main():
     print("BlackJack Program\nBlackjack payout is 3:2")
 
     deck = deckBuild()
-    keepGoing = 'y'
     random.shuffle(deck)
+    keepGoing = 'y'
 
     while keepGoing.lower() == 'y':
+        if len(deck) < 26:
+            print("\nDeck running low, reloading deck.")
+            deck = deckBuild()
+        random.shuffle(deck)
+
         dealerHand = []
         playerHand = []
         money = Decimal(db.loadMoney())
         money = money.quantize(Decimal("1.00"), ROUND_HALF_UP)
-        print(f"Money: {money}")
+        print(f"\nMoney: {money}")
         betAmount = betAccept(money)
 
         print("\nDEALER'S SHOW CARD:")
@@ -129,6 +199,15 @@ def main():
         card = dealerHand[0]
         print(f"{card[1]} of {card[0]}")
 
+        if card[2] >= 10:
+            dealerPoints = 0
+            for card in dealerHand:
+                dealerPoints += card[2]
+            if dealerPoints == 21:
+                card = dealerHand[1]
+                print(f"{card[1]} of {card[0]}")
+                print("Dealer's BlackJack!")
+
         print("\nYOUR CARDS:")
         dealCard = deck.pop(0)
         playerHand.append(dealCard)
@@ -136,6 +215,16 @@ def main():
         playerHand.append(dealCard)
         for card in playerHand:
             print(f"{card[1]} of {card[0]}")
+
+        hitStand(deck, playerHand)
+
+        print("\nDEALER'S CARDS:")
+        dealerPoints = 0
+        for card in dealerHand:
+            print(f"{card[1]} of {card[0]}")
+            dealerPoints += card[2]
+
+        dealerHitStand(deck, dealerHand, dealerPoints)
 
         pointCheck(playerHand, dealerHand, money, betAmount)
 
